27e5722dd3d156d99921fcb3fe006931ae0da5b5
Avonplays <136217534+Avonplays@users.noreply.github.com>
2023-11-06 18:19:00 +0530

    Update locks.py

--- MukeshRobot/modules/locks.py
+++ MukeshRobot/modules/locks.py
@@ -592,7 +592,7 @@ __help__ = """
 ☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
 """
 
-__mod_name__ = "📍Lᴏᴄᴋs📍"
+__mod_name__ = "Lᴏᴄᴋs"
 
 LOCKTYPES_HANDLER = DisableAbleCommandHandler("locktypes", locktypes)
 LOCK_HANDLER = CommandHandler("lock", lock, pass_args=True)  # , filters=Filters.group)
