9f3b4d222fe9f09dd61b5d33c9109ac5df72cff8
Avonplays <136217534+Avonplays@users.noreply.github.com>
2023-11-06 17:39:12 +0530

    Update blacklist_stickers.py

--- MukeshRobot/modules/blacklist_stickers.py
+++ MukeshRobot/modules/blacklist_stickers.py
@@ -509,7 +509,7 @@ __help__ = """
 ☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
 """
 
-__mod_name__ = "♨️sᴛɪᴄᴋᴇʀ♨️"
+__mod_name__ = "sᴛɪᴄᴋᴇʀ"
 
 BLACKLIST_STICKER_HANDLER = DisableAbleCommandHandler(
     "blsticker", blackliststicker, admin_ok=True
