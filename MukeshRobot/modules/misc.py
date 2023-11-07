f13cf557bb201c347526cb49c35c50c7fb31b0d2
Avonplays <136217534+Avonplays@users.noreply.github.com>
2023-11-07 18:48:23 +0530

    Update misc.py

--- MukeshRobot/modules/misc.py
+++ MukeshRobot/modules/misc.py
@@ -100,7 +100,7 @@ __help__ = """
  `/ᴄᴀsʜ 1 ᴜsᴅ ɪɴʀ`
 ᴏᴜᴛᴘᴜᴛ: `1.0 ᴜsᴅ = 75.505 ɪɴʀ`
 
-☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
+I Aᴍ A Fᴀsᴛ Aɴᴅ Aᴅᴠᴀɴᴄᴇ Bᴏᴛ.
 """
 
 ECHO_HANDLER = DisableAbleCommandHandler("echo", echo, filters=Filters.group)
@@ -109,7 +109,7 @@ MD_HELP_HANDLER = CommandHandler("markdownhelp", markdown_help)
 dispatcher.add_handler(ECHO_HANDLER)
 dispatcher.add_handler(MD_HELP_HANDLER)
 
-__mod_name__ = "Exᴛʀᴀs"
+__mod_name__ = "𝗘𝗫𝗧𝗥𝗔𝗦"
 __command_list__ = ["id", "echo"]
 __handlers__ = [
     ECHO_HANDLER,
