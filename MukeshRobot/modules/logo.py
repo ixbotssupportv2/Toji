4c2e6d0e995510a298f15a8f61291ff91f240a20
Avonplays <136217534+Avonplays@users.noreply.github.com>
2023-11-07 18:43:33 +0530

    Update logo.py

--- MukeshRobot/modules/logo.py
+++ MukeshRobot/modules/logo.py
@@ -319,7 +319,7 @@ async def lego(event):
             os.remove(fname)
 
 
-__mod_name__ = "ʟᴏɢᴏ"
+__mod_name__ = "𝗟𝗢𝗚𝗢"
 
 __help__ = """
 I can create some beautiful and attractive logo for your profile pics.
