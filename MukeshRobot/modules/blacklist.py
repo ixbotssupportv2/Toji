be0f56bc503464b074ed5e989729571975a0d669
Avonplays <136217534+Avonplays@users.noreply.github.com>
2023-11-07 17:48:51 +0530

    Update blacklist.py

--- MukeshRobot/modules/blacklist.py
+++ MukeshRobot/modules/blacklist.py
@@ -464,7 +464,7 @@ Admin only:
  ❍ /unblacklist <triggers>*:* Remove triggers from the blacklist. Same newline logic applies here, so you can remove multiple triggers at once.
  ❍ /blacklistmode <off/del/warn/ban/kick/mute/tban/tmute>*:* Action to perform when someone sends blacklisted words.
 
-☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
+I Aᴍ A Fᴀsᴛ Aɴᴅ Aᴅᴠᴀɴᴄᴇ Bᴏᴛ.
 """
 
 BLACKLIST_HANDLER = DisableAbleCommandHandler(
