8723807b5b783f650d4ffac8fa6dfbfd38c334d4
Avonplays <136217534+Avonplays@users.noreply.github.com>
2023-11-07 17:42:50 +0530

    Update animez.py

--- MukeshRobot/modules/animez.py
+++ MukeshRobot/modules/animez.py
@@ -554,7 +554,7 @@ __help__ = """
  ❍ /kayo <ᴀɴɪᴍᴇ>*:* sᴇᴀʀᴄʜ ᴀɴ ᴀɴɪᴍᴇ ᴏɴ ᴀɴɪᴍᴇᴋᴀʏᴏ.ᴄᴏᴍ
  ❍ /airing <ᴀɴɪᴍᴇ>*:* ʀᴇᴛᴜʀɴs ᴀɴɪᴍᴇ ᴀɪʀɪɴɢ ɪɴғᴏ.
 
-☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
+I Aᴍ A Fᴀsᴛ Aɴᴅ Aᴅᴠᴀɴᴄᴇ Bᴏᴛ.
 """
 
 ANIME_HANDLER = DisableAbleCommandHandler("anime", anime)
