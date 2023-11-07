af9ed65c47a40c73b833d4fa1036114c6c710e2b
Avonplays <136217534+Avonplays@users.noreply.github.com>
2023-11-07 19:42:05 +0530

    Update ud.py

--- MukeshRobot/modules/ud.py
+++ MukeshRobot/modules/ud.py
@@ -27,8 +27,8 @@ dispatcher.add_handler(UD_HANDLER)
 __help__ = """
 » /ud (text) *:* sᴇᴀʀᴄʜs ᴛʜᴇ ɢɪᴠᴇɴ ᴛᴇxᴛ ᴏɴ ᴜʀʙᴀɴ ᴅɪᴄᴛɪᴏɴᴀʀʏ ᴀɴᴅ sᴇɴᴅs ʏᴏᴜ ᴛʜᴇ ɪɴғᴏʀᴍᴀᴛɪᴏɴ.
 
-☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
+I Aᴍ A Fᴀsᴛ Aɴᴅ Aᴅᴠᴀɴᴄᴇ Bᴏᴛ.
 """
-__mod_name__ = "Uʀʙᴀɴ"
+__mod_name__ = "𝗨𝗥𝗕𝗔𝗡"
 __command_list__ = ["ud"]
 __handlers__ = [UD_HANDLER]
